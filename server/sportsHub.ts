bbaed599bbab9b40c4d2096c9da0f9a70aa7630a
Dakota Kallas <kallas.dakota@gmail.com>
2024-11-12 22:02:34 -0600

    Update sportsHub.ts

--- server/sportsHub.ts
+++ server/sportsHub.ts
@@ -24,14 +24,13 @@ class SportsHubService {
   }
 
   private async updateSportsHub() {
-    this.deskthing.sendLog(`Fetching Sports Hub data...`);
-    this.sportsHubData.games = [];
+    this.deskthing.sendLog(`Fetching Sports Hub data from Finnhub API.`);
+    this.sportsHubData = {} as SportsHubData;
 
     const url =
       'https://api-secure.sports.yahoo.com/v1/editorial/s/scoreboard?lang=en-US&region=US&ysp_redesign=1&ysp_platform=desktop&leagues=nba&date=2024-11-12&v=2&ysp_enable_last_update=1';
     const games = await this.fetchNBAData(url);
     this.sportsHubData.games = games;
-    this.deskthing.sendLog(`Sports Hub data was fetched!`);
 
     const now = new Date();
     const timeString = now.toLocaleTimeString([], {
@@ -49,63 +48,11 @@ class SportsHubService {
     });
   }
 
-  private scheduleIntervalUpdates() {
-    if (this.updateTaskId) {
-      this.updateTaskId();
-    }
-    this.updateTaskId = DeskThing.addBackgroundTaskLoop(async () => {
-      this.updateSportsHub();
-      const interval = this.refreshInterval > 0 ? this.refreshInterval : 1;
-      await this.sleep(interval * 60 * 1000);
-    }); // Update every set amount of minutes
-  }
-
-  private sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
-  public updateData(data: DataInterface) {
-    if (!data.settings) {
-      this.deskthing.sendLog('No settings defined');
-      return;
-    }
-    try {
-      this.deskthing.sendLog('Updating settings');
-      this.refreshInterval =
-        (data.settings.refreshInterval.value as number) || 5;
-      this.updateSportsHub();
-    } catch (error) {
-      this.deskthing.sendLog('Error updating Sports Hub data: ' + error);
-    }
-  }
-
-  async stop() {
-    this.lastUpdateTime = null;
-  }
-
-  public async getSportsHub(): Promise<SportsHubData> {
-    // If it's been more than {set refresh interval} since the last update, update the sports data
-    if (
-      !this.lastUpdateTime ||
-      new Date().getTime() - this.lastUpdateTime.getTime() > 15 * 60 * 1000
-    ) {
-      DeskThing.sendLog('Fetching Sports Hub data...');
-      await this.updateSportsHub();
-    }
-    DeskThing.sendLog('Returning Sports Hub data');
-    return this.sportsHubData;
-  }
-
   // Function to fetch and parse data from the API
   private async fetchNBAData(url: string): Promise<Game[]> {
     try {
-      DeskThing.sendLog('[1] Fetching Sports Hub data... (URL: ' + url + ')');
       const response = await fetch(url);
-      DeskThing.sendLog('[2] Fetching Sports Hub data...');
       if (!response.ok) {
-        this.deskthing.sendLog(
-          'Error fetching Sports Hub Data: ' + response.statusText
-        );
         throw new Error(`Error fetching data: ${response.statusText}`);
       }
 
@@ -155,10 +102,57 @@ class SportsHubService {
 
       return games;
     } catch (error) {
-      this.deskthing.sendLog('Error fetching or parsing Sports Hub Data!');
+      this.deskthing.sendError('Error fetching Sports Hub Data!');
       console.error('Error fetching or parsing data:', error);
       return []; // Return an empty array in case of error
     }
+  }
+
+  private scheduleIntervalUpdates() {
+    if (this.updateTaskId) {
+      this.updateTaskId();
+    }
+    this.updateTaskId = DeskThing.addBackgroundTaskLoop(async () => {
+      this.updateSportsHub();
+      const interval = this.refreshInterval > 0 ? this.refreshInterval : 1;
+      await this.sleep(interval * 60 * 1000);
+    }); // Update every set amount of minutes
+  }
+
+  private sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  public updateData(data: DataInterface) {
+    if (!data.settings) {
+      this.deskthing.sendLog('No settings defined');
+      return;
+    }
+    try {
+      this.deskthing.sendLog('Updating settings');
+      this.refreshInterval =
+        (data.settings.refreshInterval.value as number) || 5;
+      this.updateSportsHub();
+    } catch (error) {
+      this.deskthing.sendLog('Error updating Sports Hub data: ' + error);
+    }
+  }
+
+  async stop() {
+    this.lastUpdateTime = null;
+  }
+
+  public async getSportsHub(): Promise<SportsHubData> {
+    // If it's been more than an hour since the last update, update the sports data
+    if (
+      !this.lastUpdateTime ||
+      new Date().getTime() - this.lastUpdateTime.getTime() > 15 * 60 * 1000
+    ) {
+      DeskThing.sendLog('Fetching Sports Hub data...');
+      await this.updateSportsHub();
+    }
+    DeskThing.sendLog('Returning Sports Hub data');
+    return this.sportsHubData;
   }
 }
 
